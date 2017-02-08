6306b61d2e19478b02e222aee6b78e3de05aa597
dllo <dllo@dllodeMac-mini-109.local>
2017-02-08 20:05:26 +0800

    断剑重铸之日

---
+++ component/Main.js
@@ -0,0 +1,125 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TabBarIOS,
+    NavigatorIOS
+} from 'react-native';
+
+//引入组件
+var Home = require("./Home");
+var Message = require("./Message");
+var Find = require("./Find");
+var Mine = require("./Mine");
+
+
+
+var Main = React.createClass({
+    getInitialState:function () {
+        return{
+            Maintitl:'首页'
+        }
+    },
+
+    render:function () {
+        return(
+          <TabBarIOS
+           tintColor='orange'
+
+          >
+              {/*首页*/}
+              <TabBarIOS.Item
+               icon={require("./../images/tabbar_home@2x.png")}
+               title="首页"
+               selected={this.state.Maintitl=='首页'}
+               onPress={()=>{this.setState({
+                  Maintitl:'首页'
+              })}}
+              >
+                <NavigatorIOS
+                 tintColor='orange'
+                 style={{flex:1}}
+                 initialRoute={{
+                     component:Home,//需要控制那个板块
+                     title:'主页',
+                     leftButtonIcon:require("./../images/navigationbar_friendattention@2x.png"),
+                     rightButtonIcon:require("./../images/navigationbar_pop@2x.png")
+                 }}
+                />
+
+              </TabBarIOS.Item>
+              {/*消息*/}
+              <TabBarIOS.Item
+                  icon={require("./../images/tabbar_message_center@2x.png")}
+                  title="消息"
+                  selected={this.state.Maintitl=='消息'}
+                  onPress={()=>{this.setState({
+                  Maintitl:'消息'
+              })}}
+              >
+                  <NavigatorIOS
+                      style={{flex:1}}
+                      initialRoute={{
+                     component:Message,//需要控制那个板块
+                     title:'消息'
+                 }}
+                  />
+
+              </TabBarIOS.Item>
+              {/*发现*/}
+              <TabBarIOS.Item
+                  icon={require("./../images/tabbar_discover@2x.png")}
+                  title="发现"
+                  selected={this.state.Maintitl=='发现'}
+                  onPress={()=>{this.setState({
+                  Maintitl:'发现'
+              })}}
+              >
+                  <NavigatorIOS
+                      style={{flex:1}}
+                      initialRoute={{
+                     component:Find,//需要控制那个板块
+                     title:'发现'
+                 }}
+                  />
+
+              </TabBarIOS.Item>
+              {/*我的*/}
+              <TabBarIOS.Item
+                  icon={require("./../images/tabbar_profile@2x.png")}
+                  title="我的"
+                  selected={this.state.Maintitl=='我的'}
+                  onPress={()=>{this.setState({
+                  Maintitl:'我的'
+              })}}
+              >
+                  <NavigatorIOS
+                      style={{flex:1}}
+                      initialRoute={{
+                     component:Mine,//需要控制那个板块
+                     title:'我的'
+                 }}
+                  />
+
+              </TabBarIOS.Item>
+          </TabBarIOS>
+        );
+    }
+});
+
+
+const styles = StyleSheet.create({
+
+});
+
+//输出类
+module.exports = Main;
+
